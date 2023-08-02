8d53ffdbfcd0f46d2d9374b743eeb8337e781b1a
Quang Ngô Văn <quang@sanbul.net>
2023-08-02 11:04:11 +0700

    update module g-app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
+import { AwsModule } from './aws/aws.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/app.module';
 import { MailGoogleModule } from './mailGoogle/mailGoogle.module'
 import { MailTmModule } from './mailTemporary/mail.module';
+import config from './config/configuration';
 
 @Module({
-  imports: [PrismaModule, MailGoogleModule, AuthModule, MailTmModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    AwsModule, PrismaModule, MailGoogleModule, AuthModule, MailTmModule],
+  providers: [ConfigService]
 })
 export class AppModule { }
